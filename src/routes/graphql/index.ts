d392b5e6b9dc29750e60ed0ae9b56734a0edd201
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-07-24 21:26:59 +0300

    feat: add depth limit

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,7 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, getSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -14,17 +15,21 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
     },
+
     async handler(req) {
       const schema = getSchema(prisma);
       const reqQuery = req.body.query;
       const reqVars = req.body.variables;
+      const errors = validate(schema, parse(reqQuery), [depthLimit(5)]);
 
-      return await graphql({
-        schema,
-        source: reqQuery,
-        variableValues: reqVars,
-        contextValue: prisma,
-      });
+      return errors.length > 0
+        ? { data: null, errors }
+        : await graphql({
+            schema,
+            source: reqQuery,
+            variableValues: reqVars,
+            contextValue: prisma,
+          });
     },
   });
 };
